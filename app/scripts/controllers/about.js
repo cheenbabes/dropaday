ab817bf63ea0b28c6b522acadca6dcca16c83d9a
Eugene Baibourine <eugene.baibourine@gmail.com>
2016-12-16 11:54:36 -0700

    initial working commit with deploy

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dropadayApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dropadayApp
+ */
+angular.module('dropadayApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dropadayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dropadayApp
+ */
+angular.module('dropadayApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
