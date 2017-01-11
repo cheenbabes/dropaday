ccd01b51eefe2ff1c7e98af46df795aa5a7a6a14
Eugene Baibourine <eugene.baibourin@gmail.com>
2017-01-11 16:42:35 -0700

    changes to firebase array

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,27 +8,32 @@
  * Controller of the dropadayApp
  */
 angular.module('dropadayApp')
-    .controller('MainCtrl', function ($scope, $firebaseObject, Auth, firebase) {
+    .controller('MainCtrl', function ($scope, $firebaseObject, Auth, firebase, $firebaseArray) {
 
         $scope.auth = Auth;
 
         $scope.auth.$onAuthStateChanged(function (firebaseUser) {
             $scope.firebaseUser = firebaseUser;
-            
+
             console.log(firebaseUser)
-            
+
             var userRef = firebase.database().ref().child("users/" + firebaseUser.uid);
             var syncPledge = $firebaseObject(userRef);
-            syncPledge.$bindTo($scope, "pledge")
-            
+            syncPledge.$bindTo($scope, "pledge");
+
         });
-    
 
+        //TEST////////
         var ref = firebase.database().ref().child("data");
-
         var syncObject = $firebaseObject(ref);
-
         syncObject.$bindTo($scope, "data");
+        /////////
+    
+    
+        var userEndpoint = firebase.database().ref().child("users");
+        var userList = $firebaseArray(userEndpoint);
+    
+        $scope.userList = userList;
 
 
     });
