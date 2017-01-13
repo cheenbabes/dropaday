8c3ad10b985e9c130cfe8553d2aa63359c96f636
Eugene Baibourine <eugene.baibourin@gmail.com>
2017-01-12 21:20:46 -0700

    added async refresh for total count of pages pledged. Next step is to count the total number read that day.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,36 +10,33 @@
 angular.module('dropadayApp')
     .controller('MainCtrl', function ($scope, $firebaseObject, Auth, firebase, $firebaseArray) {
 
-        $scope.auth = Auth;
+        //Hide other mode by default
+        $scope.otherMode = false;
 
         //Gets the firebase User and allows user to make their pledge of pages
-        $scope.auth.$onAuthStateChanged(function (firebaseUser) {
+        Auth.$onAuthStateChanged(function (firebaseUser) {
             $scope.firebaseUser = firebaseUser;
 
             var userRef = firebase.database().ref().child("users/" + firebaseUser.uid);
             var syncPledge = $firebaseObject(userRef);
             syncPledge.$bindTo($scope, "pledge");
 
-            //function to report daily scores
-            //by default, submit the user's pledge.
-            //otherwise, provide input to submit his own score
-
-
-
         });
 
-        //Get the list of all signed up users and their scores
+        //Get the list of all signed up users and their scores but update it in real time via async 'on' call
         var userEndpoint = firebase.database().ref().child("users");
-        $scope.userList = $firebaseArray(userEndpoint);
+        userEndpoint.on('value', function (snapshot) {
+            $scope.userList = $firebaseArray(userEndpoint);
 
-        //tally total scores call $loaded() because async callback -- make sure entire array is loaded before reducing
-        $scope.userList.$loaded().then(function (x) {
-            $scope.totalPages = x.reduce(function (i, user) {
-                return i + parseInt(user.pages);
-            }, 0);
+            //tally total scores call $loaded() because async callback -- make sure entire array is loaded before reducing
+            $scope.userList.$loaded().then(function (x) {
+                $scope.totalPages = x.reduce(function (i, user) {
+                    return i + parseInt(user.pages);
+                }, 0);
+            });
         });
 
-        $scope.otherMode = false;
+
 
 
         //dummy confirmation, data is actualy bound and live at the endpoint.
@@ -57,41 +54,34 @@ angular.module('dropadayApp')
 
         $scope.reportDailyScore = function (pledgedPages) {
             console.log("pledged pages" + pledgedPages);
-            
-            var dailyScoresEndpoint = firebase.database().ref().child("daily");
-            var dateString = (new Date()).toISOString().slice(0, 10).replace(/-/g, "")
+            var clear = true;
+            var _pages = parseInt(pledgedPages);
+            if (isNaN(_pages)) {
+                clear = false;
+                console.log("You didnt submit a number!")
+            }
 
-            var _user = Auth.$getAuth();
-            var _userInfo = $firebaseObject(firebase.database().ref().child("users/" + _user.uid));
-        
-            _userInfo.$loaded().then(function (x){
-                console.log(x.pages);
-                var dailyScoresObject = $firebaseObject(dailyScoresEndpoint.child(dateString).child(_user.uid));
-                dailyScoresObject.pages = pledgedPages;
-                dailyScoresObject.user = _user.uid;
-                dailyScoresObject.$save().then(function (ref) {
-                    console.log("Success")
-                }, function (error) {
-                    console.log("Error:", error);
+            if (clear) {
+                console.log("Good job, it's a number")
+                var dailyScoresEndpoint = firebase.database().ref().child("daily");
+                var dateString = (new Date()).toISOString().slice(0, 10).replace(/-/g, "")
+
+                var _user = Auth.$getAuth();
+                var _userInfo = $firebaseObject(firebase.database().ref().child("users/" + _user.uid));
+
+                _userInfo.$loaded().then(function (x) {
+                    var dailyScoresObject = $firebaseObject(dailyScoresEndpoint.child(dateString).child(_user.uid));
+                    dailyScoresObject.pages = pledgedPages;
+                    dailyScoresObject.user = _user.uid;
+                    dailyScoresObject.$save().then(function (ref) {
+                        console.log("Success")
+                    }, function (error) {
+                        console.log(error);
+                    });
+
                 });
-                
-            })
-            
-            
-            //check to see if the other box has anything, if so, take that value, otherwise, pages will be read from the user pledge
-            //            var pages;
-            //            if $scope.other.pages != null and !isNaN($scope.other.pages){
-            //                pages = $scope.other.pages;
-            //            }else{
-            //                pages = _userInfo.pages;
-            //            }
+            }
 
-
-            //write to endpoint indexed by the user so multiple submissions will overwrite the same info, eg user can't submit more than 1 report per day
-            
-        
-
-            
         }
 
 
