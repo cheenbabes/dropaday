0d2ea6c221f5b4b67fdf559dde170d4f2c588ef7
Eugene Baibourine <eugene.baibourin@gmail.com>
2017-01-12 19:52:06 -0700

    working impl of adding the pages you read today- but without amends, just firebase writing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,7 +11,7 @@ angular.module('dropadayApp')
     .controller('MainCtrl', function ($scope, $firebaseObject, Auth, firebase, $firebaseArray) {
 
         $scope.auth = Auth;
-    
+
         //Gets the firebase User and allows user to make their pledge of pages
         $scope.auth.$onAuthStateChanged(function (firebaseUser) {
             $scope.firebaseUser = firebaseUser;
@@ -20,65 +20,78 @@ angular.module('dropadayApp')
             var syncPledge = $firebaseObject(userRef);
             syncPledge.$bindTo($scope, "pledge");
 
+            //function to report daily scores
+            //by default, submit the user's pledge.
+            //otherwise, provide input to submit his own score
+
+
+
         });
-    
+
         //Get the list of all signed up users and their scores
         var userEndpoint = firebase.database().ref().child("users");
         $scope.userList = $firebaseArray(userEndpoint);
-    
+
         //tally total scores call $loaded() because async callback -- make sure entire array is loaded before reducing
-        $scope.userList.$loaded().then(function(x) {
-            $scope.totalPages = x.reduce(function (i, user){
-                return i  + parseInt(user.pages);
+        $scope.userList.$loaded().then(function (x) {
+            $scope.totalPages = x.reduce(function (i, user) {
+                return i + parseInt(user.pages);
             }, 0);
         });
-    
-        
-        //function to report daily scores
-        //by default, submit the user's pledge.
-        //otherwise, provide input to submit his own score
-        var _user = $scope.auth.$getAuth();
-        var _userInfo = $firebaseObject(firebase.database().ref().child("users/" + _user.uid));
-    
-        $scope.reportDailyScore = function() {
-            var dailyScoresEndpoint = firebase.database().ref().child("daily");
-            var dateString = (new Date()).toISOString().slice(0,10).replace(/-/g,"")
-            
-            //check to see if the other box has anything, if so, take that value, otherwise, pages will be read from the user pledge
-            var pages;
-            if $scope.other.pages != null and !isNaN($scope.other.pages){
-                pages = $scope.other.pages;
-            }else{
-                pages = _userInfo.pages;
-            }
-            
-            //Add to array
-            console.log("about to add to array");
-            //write to endpoint indexed by the user so multiple submissions will overwrite the same info, eg user can't submit more than 1 report per day
-            var dailyScoresArray = $firebaseArray(dailyScoresEndpoint.child(dateString).child(_user.uid));
-            dailyScoresArray.$add({
-                //add the entire user to the object
-                user: _user,
-                pages: _userInfo.pages, //verify that this works, then change to pages
-                time: new Date()
-            });
-            
-            console.log("Successfully added to array endpoint");
-        }
-        
+
+        $scope.otherMode = false;
+
+
         //dummy confirmation, data is actualy bound and live at the endpoint.
-        $scope.showConfirmation = function(pages){
-          //Flash.create('success', "Thank you for pledging to read " + pages + " of Srila Prabhupada's books a day.");
+        $scope.showConfirmation = function (pages) {
+            //Flash.create('success', "Thank you for pledging to read " + pages + " of Srila Prabhupada's books a day.");
         };
-        
-        
-        $scope.otherAmount = function(){
+
+
+        $scope.otherAmount = function () {
             //User read some other amount than the pledge
             //Make the other input available and hide the pledge window.
             $scope.otherMode = true;
 
         }
 
+        $scope.reportDailyScore = function () {
+            var dailyScoresEndpoint = firebase.database().ref().child("daily");
+            var dateString = (new Date()).toISOString().slice(0, 10).replace(/-/g, "")
+
+            var _user = Auth.$getAuth();
+            var _userInfo = $firebaseObject(firebase.database().ref().child("users/" + _user.uid));
         
+            _userInfo.$loaded().then(function (x){
+                console.log(x.pages);
+                var dailyScoresObject = $firebaseObject(dailyScoresEndpoint.child(dateString).child(_user.uid));
+                dailyScoresObject.pages = x.pages;
+                dailyScoresObject.user = _user.uid;
+                dailyScoresObject.$save().then(function (ref) {
+                    console.log("Success")
+                }, function (error) {
+                    console.log("Error:", error);
+                });
+                
+            })
+            
+            
+            //check to see if the other box has anything, if so, take that value, otherwise, pages will be read from the user pledge
+            //            var pages;
+            //            if $scope.other.pages != null and !isNaN($scope.other.pages){
+            //                pages = $scope.other.pages;
+            //            }else{
+            //                pages = _userInfo.pages;
+            //            }
+
+
+            //write to endpoint indexed by the user so multiple submissions will overwrite the same info, eg user can't submit more than 1 report per day
+            
+        
+
+            
+        }
+
+
 
     });
